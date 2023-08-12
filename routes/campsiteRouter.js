d990949a24c1b4317fea1696cb77395077031965
Angelique <ahern29047.ah@gmail.com>
2023-08-12 13:02:49 -0500

    Week 1 Workshop Assignment

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const campsiteRouter = express.Router();
-
 campsiteRouter.route('/')
     .all((req, res, next) => {
         res.statusCode = 200;
@@ -22,9 +21,6 @@ campsiteRouter.route('/')
     });
 
 
-
-
-
 campsiteRouter.route('/:campsiteId')
     .all((req, res, next) => {
         res.statusCode = 200;
@@ -33,17 +29,19 @@ campsiteRouter.route('/:campsiteId')
     })
 
     .get((req, res) => {
-        res.end('Will send all the campsites to you');
+        res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
     })
     .post((req, res) => {
-        res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+        res.statusCode = 403;
+        res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
     })
     .put((req, res) => {
-        res.statusCode = 403;
-        res.end('PUT operation not supported on /campsites');
+        res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+        res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
     })
     .delete((req, res) => {
-        res.end('Deleting all campsites');
+        res.end(`Deleting campsite: ${req.params.campsiteId}`);
     });
 
 
--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -31,17 +31,19 @@ promotionRouter.route('/:promotionId')
     })
 
     .get((req, res) => {
-        res.end('Will send all the promotions to you');
+        res.end(`Will send details of the promotion: ${req.params.promotionId} to you`);
     })
     .post((req, res) => {
-        res.end(`Will add the promotions: ${req.body.name} with description: ${req.body.description}`);
+        res.statusCode = 403;
+        res.end(`POST operation not supported on /promtions/${req.params.promotionId}`);
     })
     .put((req, res) => {
-        res.statusCode = 403;
-        res.end('PUT operation not supported on /promotions');
+        res.write(`Updating the promotion: ${req.params.promotionId}\n`);
+        res.end(`Will update the promotion: ${req.body.name}
+            with description: ${req.body.description}`);
     })
     .delete((req, res) => {
-        res.end('Deleting all promotions');
+        res.end(`Deleting promotion: ${req.params.promotionId}`);
     });
 
 
